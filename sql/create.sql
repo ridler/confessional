01d7eede9da5f2eb17e1c4e1b6fa634dd4d6153b
ridler <tadoubleg@gmail.com>
2014-04-29 11:00:59 -0600

    updated schema

--- sql/create.sql
+++ sql/create.sql
@@ -1,27 +1,19 @@
-create table sessions (
-	ID serial primary key,
-	created_at timestamp not null,
-	banned boolean
-);
-
 create table confessions (
 	ID serial primary key,
 	created_at timestamp default current_timestamp,
 	sessionID int,
 	body text,
 	likes int default 0,
-	dislikes int default 0,
-	FOREIGN KEY (sessionID) REFERENCES sessions(ID)
+	dislikes int default 0
 );
 
 create table comments (
 	ID serial primary key,
-	created_at timestamp not null,
+	created_at timestamp default current_timestamp,
 	confessionID int,
 	sessionID int,
 	body text,
 	likes int default 0,
 	dislikes int default 0,
-	FOREIGN KEY (confessionID) REFERENCES confessions(ID),
-	FOREIGN KEY (sessionID) REFERENCES sessions(ID)
+	FOREIGN KEY (confessionID) REFERENCES confessions(ID)
 );
